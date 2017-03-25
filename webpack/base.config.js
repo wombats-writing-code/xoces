90bdd49ebf7199dec57f5f3cb1282631250540a0
luwen-huang <huang.luwen@gmail.com>
2017-03-25 12:19:19 -0400

    postcss loading

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -31,7 +31,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader', ]
       },
       { test: /\.png$/, loader: "url-loader?mimetype=image/png" }
     ],
