ae85e03deb2b0ec04d591978067fbd30d4d0e586
luwen-huang <huang.luwen@gmail.com>
2017-05-30 08:21:15 -0400

    update build test

--- webpack/__tests__/webpack.spec.js
+++ webpack/__tests__/webpack.spec.js
@@ -10,28 +10,29 @@ const DIST_PATH = path.resolve(__dirname, '../../dist')
 describe('dist build targets', function() {
   this.timeout(5000);
 
-  it('should have the xoces lib in UMD target ', () => {
-    let xoces = require(`${DIST_PATH}/xoces-umd`)
+  it('should have the xoces lib in UMD target ', (done) => {
+    let xoces = require(`${DIST_PATH}/umd/xoces-umd`)
 
-    console.log('\n xoces UMD', xoces, '\n');
-
-    xoces.should.include.keys('widgets')
-  })
-
-  it('should have the xoces lib in CommonJS target ', () => {
-    let xoces = require(`${DIST_PATH}/xoces-commonjs2`)
-
-    console.log('\n xoces commonjs2', xoces, '\n');
+    // console.log('\n xoces UMD', xoces, '\n');
 
     xoces.should.include.keys('widgets')
+    done();
   })
 
-  it('should have the xoces lib in var target ', () => {
-    let xoces = require(`${DIST_PATH}/xoces-var`)
-
-    console.log('\n xoces var', xoces, '\n');
-
-    xoces.should.include.keys('widgets')
-  })
+  // it('should have the xoces lib in CommonJS target ', () => {
+  //   let xoces = require(`${DIST_PATH}/xoces-commonjs2`)
+  //
+  //   console.log('\n xoces commonjs2', xoces, '\n');
+  //
+  //   xoces.should.include.keys('widgets')
+  // })
+  //
+  // it('should have the xoces lib in var target ', () => {
+  //   let xoces = require(`${DIST_PATH}/xoces-var`)
+  //
+  //   console.log('\n xoces var', xoces, '\n');
+  //
+  //   xoces.should.include.keys('widgets')
+  // })
 
 })
